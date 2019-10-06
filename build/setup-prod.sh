4826402bdb4432318da96584e24568cefd3d1d93
RSN <rajendra.mitu06@gmail.com>
2019-10-06 08:42:21 +0530

    Check Build #8

--- build/setup-prod.sh
+++ build/setup-prod.sh
@@ -5,4 +5,4 @@ echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid '3MVG9pe2TCoA1Pf5A3gfcG6XYI3xg93lDMW4N8HE6CF7SUPaJKHB2Pd4_IQp2zO8D9eeLMpBu3E3nX6yPwQL1' --jwtkeyfile keys/server.key --username 'rajendra.nagar@tools.com' --setdefaultdevhubusername -a DevHub
